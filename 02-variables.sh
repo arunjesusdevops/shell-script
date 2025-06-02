ae5f08204c83d159b8fa523c8a6e99824be423f7
arunjesusdevops <postgresarun@gmail.com>
2025-06-02 16:46:25 +0530

    added chnages

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Hello Suresh, Good morning"
-echo "HI Ramesh, Good morning"
-echo "How are you Suresh"
-echo "I am good Ramesh. Thank you"
+PERSON1=Sam
+PERSON2=Mahesh
+echo "Hello $PERSON1, Good morning"
+echo "HI $PERSON2, Good morning"
+echo "How are you $PERSON1"
+echo "I am good $PERSON2. Thank you"
---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username"
+
+read -s username
+
+echo "Please enter password"
+
+read -s password
+
+echo "username is $USERNAME and password is $PASSWORD"
