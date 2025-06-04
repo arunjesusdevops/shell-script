44e317f6607cb412bacc2e73a44a35e2dd10d7b1
arunjesusdevops <postgresarun@gmail.com>
2025-06-04 18:21:38 +0530

    updated new mysql script -v2

--- 10-git0mysql.sh
+++ 10-git0mysql.sh
@@ -4,16 +4,17 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ];
 then
-    echo "ERROR:: Please run the script with root id"
+    echo "ERROR:: Please run the script with root access"
+    exit 1 
 else
-    echo "You are with root user"
+    echo "You are root user"
 fi
 
 yum install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "MySQL not installed successfully"
+    echo "Installing MYSQL failed"
     exit 1
 else
     ehco "MySQL installed successfully"
@@ -23,7 +24,7 @@ yum install git -y
 
 if [ $? -ne 0 ;
 then
-    echo "Installed GIT no installed"
+    echo "Installing MYSQL Failed"
     exit 1
 else
     echo "GIT installed successfully"
